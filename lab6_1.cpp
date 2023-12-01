6953e23dd211427ad282a9e6afa50cfe92fb244f
Phuwanat <phuwanat_ta@cmu.ac.th>
2023-12-01 15:58:57 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,28 @@
 using namespace std;
 
 int main(){
+    int integer;
+    int x=0;
+    int y=0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> integer;
+
+    while (integer != 0)
+    {
+        if(integer%2 == 0){
+            x =x+1;
+            
+
+        }else{
+            y=y+1;
+            
+        }
+    cout << "Enter an integer: ";
+    cin >> integer;
+    }
+    
+    cout << "#Even numbers = "<< x<<"\n"; 
+    cout << "#Odd numbers = " << y;
+    
     return 0;
 }
