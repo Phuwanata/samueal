9e15c9823a2dd26e19840522b94f2f3374308e8b
Phuwanat <phuwanat_ta@cmu.ac.th>
2023-12-01 16:42:23 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+double deg2rad(double x){
+    return M_PI*x/180;
+    }
+double rad2deg(double y){
+    return y*180/M_PI;
+    }
+double findXComponent(double l1,double l2,double a1,double a2){
+    return cos(a1)*l1 + cos(a2)*l2;
+    
+    }
+double findYComponent(double l1,double l2,double a1,double a2){
+    return sin(a1)*l1 + sin(a2)*l2; 
+    } 
+double pythagoras(double x,double y){
+     return  sqrt(pow(x,2)+pow(y,2));
+    }
+double showResult(double a,double b){
+    cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<"\n";
+    cout<<"Length of the resultant vector = "<< a <<"\n" ;
+    cout<<"Direction of the resultant vector (deg) = "<<  b <<endl ;
+    cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<"\n";
+    
+    return 0;
+    }
+
+
+
 int main(){
+    
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
     cout << "Enter length of the first vector: ";
     cin >> l1;
